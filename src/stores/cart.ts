8d1c57796a1bf337ebb301d1e1f82742446345de
kaltepeter <kayla.altepeter@datasite.com>
2023-05-30 11:42:24 -0500

    initial setup

---
+++ src/stores/cart.ts
@@ -0,0 +1,36 @@
+import { computed, map } from 'nanostores';
+
+export const $cart = map<Record<number, CartItem>>({});
+
+export function addItemToCart(item: ShopItem) {
+	const cartItem = $cart.get()[item.id];
+	const quantity = cartItem ? cartItem.quantity : 0;
+
+	$cart.setKey(item.id, {
+		item,
+		quantity: quantity + 1,
+	});
+}
+
+export function removeItemFromCart(itemId: number) {
+	// @ts-ignore
+	$cart.setKey(itemId, undefined);
+}
+
+export const subtotal = computed($cart, (entries) => {
+	// let subtotal = 0;
+	// Object.values(entries).forEach((entry) => {
+	// 	if (!entry) return;
+
+	// 	subtotal += entry.item.price * entry.quantity;
+	// });
+
+	// return subtotal;
+
+	return Object.values(entries).reduce((acc, entry) => {
+		if (!entry) return acc;
+
+		acc += entry.item.price * entry.quantity;
+		return acc;
+	}, 0);
+});
